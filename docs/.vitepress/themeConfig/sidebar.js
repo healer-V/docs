2c99b90a5ce195fe296e8b5c6a1d484475ca0706
LHJ <wangyi2363929143@163.com>
2024-09-11 22:02:45 +0800

    更改侧边栏

--- docs/.vitepress/themeConfig/sidebar.js
+++ docs/.vitepress/themeConfig/sidebar.js
@@ -1,70 +1,87 @@
-export const sidebar = [
-    {
-        text: '站点介绍',
-        link:'../../../Home.md'
-    },
-    {
-        text: '前端基础',
-        items: [
-            { text: 'HTML', link: '/SideBar/Basic/HTML.md' },
-            { text: 'CSS', link: '/SideBar/Basic/CSS.md' },
-            { text: 'Javascript', link: '/SideBar/Basic/Javascript.md' },
-            { text: 'TypeScript', link: '/SideBar/Basic/TypeScript.md' },
-        ]
-    },
-    {
-        text: '网络相关',
-        items: [
-            { text: 'HTTP', link: '/SideBar/Network/HTTP.md' },
-            { text: 'UDP/TCP', link: '/SideBar/Network/UDP&TCP.md' },
-        ]
-    },
-    {
-        text: '浏览器相关',
-        items: [
-            { text: 'Git', link: '/SideBar/Tools/git' },
-            { text: 'Markdown', link: '/SideBar/Tools/markdown' },
-            { text: 'VSCode', link: '/SideBar/Tools/vscode' },
-            { text: 'Typora', link: '/SideBar/Tools/typora' },
-            { text: 'PicGo', link: '/SideBar/Tools/picgo' },
-        ]
-    },
-    {
-        text: '框架',
-        items: [
-            { text: 'Vue', link: '/SideBar/Frame/Vue.md' },
-            { text: 'React', link: '/SideBar/Frame/React.md' },
-            { text: 'Angular', link: '/SideBar/Frame/Angular.md' },
-            { text: 'Uniapp', link: '/SideBar/Frame/Uniapp.md' },
-            { text: 'Flutter', link: '/SideBar/Frame/Flutter.md' },
-            { text: 'React Native', link: '/SideBar/Frame/ReactNative.md' },
+export const sidebar =
+    [
+        {
+            text: '站点介绍',
+            link: '../../../Home.md'
+        },
+        {
+            text: '前端基础',
+            collapsible: true,
+            items: [
+                { text: 'HTML', link: '/articles/basic/html.md' },
+                { text: 'CSS', link: '/articles/basic/css.md' },
+                { text: 'Javascript', link: '/articles/basic/javascript.md' },
+                { text: 'TypeScript', link: '/articles/basic/typescript.md' },
+            ]
+        },
+        {
+            text: '网络相关',
+            collapsible: true,
+            items: [
+                { text: 'HTTP', link: '/articles/network/http.md' },
+                { text: 'UDP/TCP', link: '/articles/network/udp&tcp.md' },
+            ]
+        },
+        {
+            text: '浏览器相关',
+            collapsible: true,
+            items: [
+                { text: 'Git', link: '/articles/browser/git' },
+                { text: 'Markdown', link: '/articles/browser/markdown' },
+                { text: 'VSCode', link: '/articles/browser/vscode' },
+            ]
+        },
+        {
+            text: '框架',
+            collapsible: true,
+            items: [
+                { text: 'Vue', link: '/articles/frame/Vue.md' },
+                { text: 'React', link: '/articles/frame/React.md' },
+                { text: 'Angular', link: '/articles/frame/Angular.md' },
+                { text: 'Uniapp', link: '/articles/frame/Uniapp.md' },
+                { text: 'Flutter', link: '/articles/frame/Flutter.md' },
+                { text: 'React Native', link: '/articles/frame/ReactNative.md' },
 
-        ]
-    },
-    {
-        text: '前端工程化',
-        items: [
-            { text: 'Webpack', link: '/SideBar/Engineering/git.md' },
-            { text: 'Vite', link: '/SideBar/Engineering/markdown.md' },
-            { text: 'ESBuild', link: '/SideBar/Engineering/vscode.md' },
-            { text: 'Rollup', link: '/SideBar/Engineering/typora.md' },
-            { text: 'Rolldown', link: '/SideBar/Engineering/typora.md' },
-        ]
-    },
-    {
-        text: '工具',
-        items: [
-            { text: 'Git', link: '/SideBar/Tools/Git.md' },
-            { text: 'Markdown', link: '/SideBar/Tools/Markdown.md' },
-            { text: 'VSCode', link: '/SideBar/Tools/Vscode.md' },
-        ]
-    },
+            ]
+        },
+        {
+            text: '前端工程化',
+            collapsible: true,
+            items: [
+                { text: 'Webpack', link: '/articles/engineering/Webpack.md' },
+                { text: 'Vite', link: '/articles/engineering/Vite.md' },
+                { text: 'ESBuild', link: '/articles/engineering/ESBuild.md' },
+                { text: 'Rollup', link: '/articles/engineering/Rollup.md' },
+                { text: 'Rolldown', link: '/articles/engineering/Rolldown.md' },
+            ]
+        },
+        {
+            text: '工具',
+            collapsible: true,
+            items: [
+                { text: 'Git', link: '/articles/tools/Git.md' },
+                { text: 'Markdown', link: '/articles/tools/Markdown.md' },
+                { text: 'VSCode', link: '/articles/tools/Vscode.md' },
+            ]
+        },
 
-    {
-        text: '关于',
-        items: [
-            { text: '关于我', link: '/SideBar/About/me' },
-            { text: '友情链接', link: '/SideBar/About/links' },
-        ]
-    },
-]
+        {
+            text: '关于',
+            collapsible: true,
+            items: [
+                { text: '关于我', link: '/articles/about/me' },
+                { text: '友情链接', link: '/articles/about/links' },
+            ]
+        },
+    ]
+// '/blog/': [
+//     {
+//         text: 'Config',
+//         items: [
+//             // This shows `/config/index.md` page.
+//             { text: 'Index', link: '/config/' }, // /config/index.md
+//             { text: 'Three', link: '/config/three' }, // /config/three.md
+//             { text: 'Four', link: '/config/four' } // /config/four.md
+//         ]
+//     }
+// ]
